1cde4333980789a2dccb74a540279d79b636298e
nicoco <nicoco@nicoco.fr>
2025-03-28 06:05:52 +0100

    build: check if the go executable is present in $PATH

--- build.py
+++ build.py
@@ -1,11 +1,17 @@
 # build script for whatsapp extensions
 
 import os
+import shutil
 import subprocess
 from pathlib import Path
 
 
 def main():
+    if not shutil.which("go"):
+        raise RuntimeError(
+            "Cannot find the go executable in $PATH. "
+            "Make you sure install golang, via your package manager or https://go.dev/dl/"
+        )
     os.environ["PATH"] = os.path.expanduser("~/go/bin") + ":" + os.environ["PATH"]
     subprocess.run(["go", "install", "github.com/go-python/gopy@master"], check=True)
     subprocess.run(
