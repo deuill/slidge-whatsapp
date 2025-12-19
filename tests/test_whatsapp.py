139d53663606290f03301e908ed3c00d4bc1d639
nicoco <nicoco@nicoco.fr>
2025-12-19 19:58:59 +0100

    fix: handle LID mentions
    Fixes: https://codeberg.org/slidge/slidge-whatsapp/issues/64

--- tests/test_whatsapp.py
+++ tests/test_whatsapp.py
@@ -7,23 +7,23 @@ def test_replace_whatsapp_mentions():
     assert (
         replace_whatsapp_mentions(
             text,
-            {"+1234": "bibi", "+5678": "baba"},
+            {"1234": "bibi", "5678": "baba"},
         )
         == "Hayo bibi, it's cool in here in with baba!! @123333"
     )
 
     assert replace_whatsapp_mentions(text, {}) == text
 
-    assert replace_whatsapp_mentions(text, {"+123333": "prout"}) == text.replace(
+    assert replace_whatsapp_mentions(text, {"123333": "prout"}) == text.replace(
         "@123333", "prout"
     )
 
     assert (
-        replace_whatsapp_mentions("+1234", {"+1234": "bibi", "+5678": "baba"})
+        replace_whatsapp_mentions("+1234", {"1234": "bibi", "5678": "baba"})
         == "+1234"
     )
 
     assert (
-        replace_whatsapp_mentions("@1234@1234@123", {"+1234": "bibi", "+5678": "baba"})
+        replace_whatsapp_mentions("@1234@1234@123", {"1234": "bibi", "5678": "baba"})
         == "bibibibi@123"
     )
